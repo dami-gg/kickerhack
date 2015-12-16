4cbb592d0c991102636be6ff1b008393b40d189e
Daniel Karch <daniel.karch@zalando.de>
2015-12-16 09:46:35 +0100

    more endpoints implemented

--- backend/app/controllers/Application.scala
+++ backend/app/controllers/Application.scala
@@ -5,46 +5,30 @@ import java.util.UUID
 import model._
 import org.joda.time.DateTime
 import play.api._
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.{Writes, JsObject, Json}
 import play.api.mvc._
 import model.JsonConversions._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def health = Action {
+    Ok("Alright then.")
   }
-
-  val mockUsers = Map(1l-> User(UserId(1), "Daniel"), 2l -> User(UserId(2), "Paul"), 3l -> User(UserId(3), "Salomé"), 4l -> User(UserId(4), "Roman"))
-  val mockTable = model.Table(id = TableId(567), name = "theTable", colorHome = Color("Blue"), building = "BMO", floor = "101", lastGoalScored = DateTime.now(), colorAway = Color("Red"))
-  val mockTables = Map(567l -> mockTable)
 
   //
   // Users
   //
-  def getUsers = Action(Ok(JsObject(Map("users" -> Json.toJson(mockUsers.values.toList)))))
-
-  def getUser(userId: Long) = Action {
-    if (mockUsers contains userId) {
-      Ok(Json.toJson(mockUsers(userId)))
-    } else {
-      NotFound
-    }
-  }
+  val mockUsers = Map(1l-> User(UserId(1), "Daniel"), 2l -> User(UserId(2), "Paul"), 3l -> User(UserId(3), "Salomé"), 4l -> User(UserId(4), "Roman"))
+  def getUsers = getAll(mockUsers, "users")
+  def getUser(userId: Long) = getById(userId, mockUsers)
 
   //
   // Tables
   //
-  def getTables = Action(Ok(JsObject(Map("tables" -> Json.toJson(mockTables.values.toList)))))
-
-  def getTable(tableId: Long) = Action {
-    if (mockTables contains tableId) {
-      val table = mockTables(tableId)
-      Ok(Json.toJson(table))
-    } else {
-      NotFound("Not found: " + tableId)
-    }
-  }
+  val mockTable = model.Table(id = TableId(567), name = "theTable", colorHome = Color("Blue"), building = "BMO", floor = "101", lastGoalScored = DateTime.now(), colorAway = Color("Red"))
+  val mockTables = Map(567l -> mockTable)
+  def getTables = getAll(mockTables, "tables")
+  def getTable(tableId: Long) = getById(tableId, mockTables)
 
   //
   // Games
@@ -52,22 +36,29 @@ class Application extends Controller {
   val mockPlayers = List(Player(UserId(1), Attack, Home), Player(UserId(2), Defense, Home),
     Player(UserId(3), Attack, Away), Player(UserId(4), Defense, Away))
   val mockGame = Game(GameId(999), TableId(567), mockPlayers, 5, 3, DateTime.now().minusMinutes(10), DateTime.now())
-  val mockGames = List(mockGame)
-  def getGames = Action(Ok(JsObject(Map("games" -> Json.toJson(mockGames)))))
-  def getGame(gameId: Long) = Action {
-    if (mockTables contains gameId) {
-      val table = mockTables(gameId)
-      Ok(Json.toJson(table))
-    } else {
-      NotFound("Not found: " + gameId)
-    }
-  }
+  val mockGames = Map(999l -> mockGame)
+  def getGames = getAll(mockGames, "games")
+  def getGame(gameId: Long) = getById(gameId, mockGames)
+
 
   //
   // NFC data
   //
   val mockNfcData = NfcData(UUID.fromString("de305d54-75b4-431b-adb2-eb6b9e546014"), TableId(567), Home, Attack)
   val mockNfcDatas = Map("de305d54-75b4-431b-adb2-eb6b9e546014" -> mockNfcData)
-  def getNfcDatas = TODO
-  def getNfcData(uuid: UUID) = TODO
+  def getNfcDatas = getAll(mockNfcDatas, "nfc-data")
+  def getNfcData(uuid: String) = getById(uuid, mockNfcDatas)
+
+  def getAll[T](all: Map[_,T], desc: String)(implicit writer: Writes[T]) = Action(Ok(JsObject(Map(desc -> Json.toJson(all.values.toList)))))
+
+  def getById[I,T](id: I, map: Map[I,T])(implicit writer: Writes[T]) = Action {
+    if (map contains id) {
+      val t = map(id)
+      Ok(Json.toJson(t))
+    } else {
+      NotFound("Not found: " + id)
+    }
+  }
+
+  def getCurrentGame(tableId: Long) = getById(999l, mockGames)
 }
