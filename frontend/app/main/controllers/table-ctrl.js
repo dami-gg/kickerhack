7a41fc16e10895de78c66848c2f1d639b50815e4
Damián García <damian.garcia@zalando.de>
2015-12-16 10:02:22 +0100

    Adapt tables view to new API definition

---
+++ frontend/app/main/controllers/table-ctrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('main')
+  .controller('TableController', ['$stateParams', 'TablesService',
+    function ($stateParams, TablesService) {
+
+      var vm = this;
+
+    }]);
