9aea2dc20698ed7d687f5b62b3f204ab9a199359
Damián García <damian.garcia@zalando.de>
2015-12-16 10:58:47 +0100

    Integrate game view with NFC check-in and with new API definition

--- frontend/app/main/controllers/check-in-ctrl.js
+++ frontend/app/main/controllers/check-in-ctrl.js
@@ -1,41 +1,37 @@
 'use strict';
 angular.module('main')
-  .controller('CheckInController', ['Config', '$http', '$timeout', '$stateParams', '$log', 
-    function (Config, $http, $timeout, $stateParams, $log) {
-    var self = this;
-    var baseUrl = Config.ENV.SERVER_URL;
-    var tagDataUrl = baseUrl 
-      + '/nfc-data/' 
-      + this.tagId;
+  .controller('CheckInController', ['Config', '$http', '$timeout', '$stateParams', '$state', '$log', 'TablesService',
+    function (Config, $http, $timeout, $stateParams, $state, $log, TablesService) {
+      var vm = this;
+      var baseUrl = Config.ENV.SERVER_URL;
+      var tagDataUrl = baseUrl
+        + '/nfc-data/'
+        + this.tagId;
 
-    this.tagId = $stateParams.tagId;
+      this.tagId = $stateParams.tagId;
 
-    this.fallbackTagData = function (error) {
-      $log.log('fallback');
-      var json = '{"uuid": "9f5b5bf9-7e74-44cb-b9d3-f915224a6a44", "table_id": 2, "side": "home", "position": "offense"}';
-      $timeout(function () {
-        self.tagData = JSON.parse(json);
-      }, 2000);
-    };
+      this.fallbackTagData = function (error) {
+        $log.log('fallback');
+        var json = '{"uuid": "9f5b5bf9-7e74-44cb-b9d3-f915224a6a44", "table_id": 2, "side": "home", "position": "offense"}';
+        $timeout(function () {
+          vm.tagData = JSON.parse(json);
+        }, 2000);
+      };
 
-    this.checkIn = function () {
-      $log.log('go..');
-      
-      var registerUrl = baseUrl 
-        + '/tables/' 
-        + self.tagData.table_id 
-        + '/current_game';
-      var registerData = self.tagData;
-      $http.put(registerUrl, registerData).then(function (response) {
-        $log.log(response.status);
-      }, function (error) {
-        $log.log(error);
-      });
+      this.checkIn = function () {
+        TablesService.registerPlayerInTable(vm.tagData.table_id, 'home', 'position') // TODO Replace hardcoded values
+          .then(function (gameId) {
+            if (gameId !== null) {
+              $state.go('main.game', {'gameId': gameId});
+            }
+            else {
+              // TODO Handle error
+            }
+          });
+      };
 
-    };
-    
-    $http.get(tagDataUrl).then(function (response) {
-      self.tagData = response.data;
-    }, this.fallbackTagData);
+      $http.get(tagDataUrl).then(function (response) {
+        vm.tagData = response.data;
+      }, this.fallbackTagData);
 
-  }]);
+    }]);
--- frontend/app/main/services/tables-serv.js
+++ frontend/app/main/services/tables-serv.js
@@ -7,6 +7,10 @@ angular.module('main')
 
     var allTablesJson = '[{"id":123,"building":"BNB","floor":1,"home":"red","away":"blue","last_goald_scored":"2015-12-14T15:20+01:00"},{"id":124,"building":"BMO","floor":1,"home":"red","away":"blue","last_goald_scored":"2015-12-14T15:20+01:00"},{"id":456,"building":"BMO","floor":2,"home":"red","away":"blue","last_goald_scored":"2015-12-14T15:20+01:00"}]';
 
+    /**
+     * Get all existing tables
+     * @returns {*}
+     */
     vm.getTables = function () {
       return $http.get('/tables')
         .then(
@@ -19,6 +23,11 @@ angular.module('main')
           });
     };
 
+    /**
+     * Get specific table data based on its id
+     * @param tableId
+     * @returns {*}
+     */
     vm.getTableById = function (tableId) {
       return $http.get('/tables/' + tableId)
         .then(
@@ -31,6 +40,10 @@ angular.module('main')
           });
     };
 
+    /**
+     * Get all the locations where there are tables
+     * @returns {*}
+     */
     vm.getLocations = function () {
       var locations = [];
       var tables = [];
@@ -60,6 +73,11 @@ angular.module('main')
         );
     };
 
+    /**
+     * Get all tables present in a certain location
+     * @param location
+     * @returns {*}
+     */
     vm.getTablesByLocation = function (location) {
       var tables = [];
       var locationTablesByFloor = [];
@@ -67,7 +85,7 @@ angular.module('main')
 
       return vm.getTables()
         .then(
-          function(response) {
+          function (response) {
             if (response !== null) {
               tables = response;
             }
@@ -95,25 +113,85 @@ angular.module('main')
 
             return locationTablesByFloor;
           },
-          function(error) {
+          function (error) {
             // TODO Handle error
             return null;
           }
         );
     };
 
-    vm.isTableFree = function(tableId) {
+    /**
+     * Get current game being played in a certain table
+     * @param tableId
+     * @returns {*} promise with result:
+     *  - current game object
+     *  - null if there's no game at the moment or an error happens
+     */
+    vm.getCurrentGameInTable = function (tableId) {
       return $http.get('/tables/' + tableId + '/current_game')
         .then(
           function (response) {
             if (response.status === 200) {
-              return false;
+              return response.data;
             }
             else if (response.status === 404) {
-              return true;
+              return null;
             }
             else {
               // TODO Handle unexpected response type
+              return null;
+            }
+          },
+          function (error) {
+            // TODO Handle error
+            return null;
+          });
+    };
+
+    /**
+     * Registers a user as a player in a table
+     * @param tableId
+     * @param side
+     * @param position
+     * @returns {*} promise with result:
+     *  - id of the game if the registration was successful
+     *  - null in case of error
+     */
+    vm.registerPlayerInTable = function (tableId, side, position) {
+      return $http.put('/tables/' + tableId + '/current_game',
+        {'side': side, 'position': position})
+        .then(
+          function (response) {
+            if (response.status === 200 || response.status === 201) {
+              return response.data;
+            }
+            else {
+              // TODO Handle error
+              return null;
+            }
+          },
+          function (error) {
+            // TODO Handle error
+            return null;
+          });
+    };
+
+    /**
+     * Registers a goal from one side in a certain table
+     * @param tableId
+     * @param side
+     * @returns {*} promise with result:
+     *  - true if the registration was successful
+     *  - false in case of error
+     */
+    vm.registerGoalInTable = function (tableId, side) {
+      return $http.post('/tables/' + tableId + '/sides/' + side + '/goal')
+        .then(function (response) {
+            if (response.status === 201) {
+              return true;
+            }
+            else {
+              // TODO Handle error
               return false;
             }
           },
@@ -121,9 +199,15 @@ angular.module('main')
             // TODO Handle error
             return false;
           });
-    };
+    }
 
-    function compare (a, b) {
+    /**
+     * Helper method to sort items
+     * @param a
+     * @param b
+     * @returns {number}
+     */
+    function compare(a, b) {
       if (a < b) {
         return -1;
       }
