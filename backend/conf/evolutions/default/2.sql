746607e510a504d9b782969b36ca88c4c1670db3
rbobin <roman.bobin@gmail.com>
2015-12-16 18:24:54 +0100

    Fixed SQL scripts

--- backend/conf/evolutions/default/2.sql
+++ backend/conf/evolutions/default/2.sql
@@ -1,11 +1,18 @@
-
 # --- !Ups
 
-create table "kicker"."game" (g_id BIGSERIAL NOT NULL PRIMARY KEY);
-create table "kicker"."player" (p_id BIGSERIAL NOT NULL PRIMARY KEY, p_user_id BIGINT NOT NULL REFERENCES user(u_id), p_game_id BIGINT NOT NULL REFERENCES game(g_id), p_position VARCHAR(254) NOT NULL, p_side VARCHAR(254) NOT NULL);
+CREATE TABLE "kicker"."game" (
+  g_id BIGSERIAL NOT NULL PRIMARY KEY
+);
+CREATE TABLE "kicker"."player" (
+  p_id       BIGSERIAL    NOT NULL PRIMARY KEY,
+  p_user_id  BIGINT       NOT NULL REFERENCES "kicker"."user" (u_id),
+  p_game_id  BIGINT       NOT NULL REFERENCES "kicker"."game" (g_id),
+  p_position VARCHAR(254) NOT NULL,
+  p_side     VARCHAR(254) NOT NULL
+);
 
 
 # --- !Downs
 
-drop table "kicker"."player";
-drop table "kicker"."game";
+DROP TABLE "kicker"."player";
+DROP TABLE "kicker"."game";
