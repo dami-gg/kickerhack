6945146d0b38ae1c662281a76e75a3f232e0ec50
Paul Fournel <paul.fournel@zalando.de>
2015-12-18 10:55:08 +0100

    finished player registration

--- backend/app/controllers/NfcDataController.scala
+++ backend/app/controllers/NfcDataController.scala
@@ -8,13 +8,12 @@ import org.joda.time.DateTime
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import repository._
-import service.AuthService
 import service.AuthServiceImpl
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import scala.util.{Success, Failure, Try}
+import scala.util.{Failure, Success}
 
 class NfcDataController @Inject()(authService: AuthServiceImpl, nfcDataRepository: NfcDataRepository, gamesRepository: GamesRepository, playerRepository: PlayerRepository) extends Controller {
 
@@ -34,39 +33,39 @@ class NfcDataController @Inject()(authService: AuthServiceImpl, nfcDataRepositor
   }
 
   def registerPlayer(uuid: String) = Action { request =>
-      var returnCode = Ok("Game created")
-      val currentUser = Await.ready(authService.auth(request), Duration.Inf).value.get match {
+    var returnCode = Ok("Game created")
+    val currentUser = Await.ready(authService.auth(request), Duration.Inf).value.get match {
+      case Success(t) => t
+      case Failure(e) => throw new IllegalArgumentException();
+    }
+    val nfcData = Await.ready(nfcDataRepository.getNfcData(uuid), Duration.Inf).value.get match {
+      case Success(t) => t.getOrElse(throw new IllegalArgumentException())
+      case Failure(e) => throw new IllegalArgumentException();
+    }
+    val game = Await.ready(gamesRepository.findCurrentGameForTable(nfcData.tableId), Duration.Inf).value.get match {
+      case Success(t) => t
+      case Failure(e) => throw new IllegalArgumentException();
+    }
+    if (game.isEmpty) {
+      val newGame = Game(None, nfcData.tableId, 0, 0, DateTime.now(), None)
+      val newGameId = Await.ready(gamesRepository.insert(newGame), Duration.Inf).value.get match {
         case Success(t) => t
         case Failure(e) => throw new IllegalArgumentException();
       }
-      val nfcData = Await.ready(nfcDataRepository.getNfcData(uuid), Duration.Inf).value.get match {
-        case Success(t) => t.getOrElse(throw new IllegalArgumentException())
-        case Failure(e) => throw new IllegalArgumentException();
-      }
-      val game = Await.ready(gamesRepository.findCurrentGameForTable(nfcData.tableId), Duration.Inf).value.get match {
+      playerRepository.insert(Player(None, currentUser.id.get, newGameId.id.get, nfcData.position, nfcData.side))
+    } else {
+      val players = Await.ready(playerRepository.findbyGame(game.get.id.get), Duration.Inf).value.get match {
         case Success(t) => t
         case Failure(e) => throw new IllegalArgumentException();
       }
-      if(game.isEmpty){
-        val newGame = Game(None, nfcData.tableId, 0, 0, DateTime.now(), None)
-        val newGameId = Await.ready(gamesRepository.insert(newGame), Duration.Inf).value.get match {
-          case Success(t) => t
-          case Failure(e) => throw new IllegalArgumentException();
-        }
-        playerRepository.insert(Player(None, currentUser.id.get, newGameId.id.get, nfcData.position, nfcData.side))
-      }else{
-        val players = Await.ready(playerRepository.findbyGame(game.get.id.get), Duration.Inf).value.get match {
-          case Success(t) => t
-          case Failure(e) => throw new IllegalArgumentException();
-        }
-        val player = players.find(p => p.position == nfcData.position && p.side == nfcData.side)
-        if(player.isEmpty){
-          playerRepository.insert(Player(None, currentUser.id.get, game.get.id.get, nfcData.position, nfcData.side))
-        }else{
-          returnCode = BadRequest("Position is already used")
-        }
+      val player = players.find(p => p.position == nfcData.position && p.side == nfcData.side)
+      if (player.isEmpty) {
+        playerRepository.insert(Player(None, currentUser.id.get, game.get.id.get, nfcData.position, nfcData.side))
+      } else {
+        returnCode = BadRequest("Position is already used")
       }
+    }
 
-      returnCode
+    returnCode
   }
 }
