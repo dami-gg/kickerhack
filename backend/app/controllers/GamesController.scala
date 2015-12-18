72241d025680cd00f36806b0fbf22f998bfcba07
Daniel Karch <daniel.karch@zalando.de>
2015-12-18 12:20:41 +0100

    stuff

--- backend/app/controllers/GamesController.scala
+++ backend/app/controllers/GamesController.scala
@@ -2,24 +2,62 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.libs.json.Json
+import model.{Player, Game}
+import play.api.libs.json
+import play.api.libs.json.{JsValue, Writes, Json}
 import play.api.mvc.{Action, Controller}
-import repository.GamesRepository
+import repository.{PlayerRepository, GamesRepository}
 import model.JsonConversions.gameFormat
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-class GamesController @Inject()(gamesRepo: GamesRepository) extends Controller {
+class GamesController @Inject()(gamesRepo: GamesRepository,
+                                playerRepo: PlayerRepository) extends Controller {
 
   def getGames = Action.async {
-    gamesRepo.list().map(
-      games => Ok(Json.obj("games" -> Json.toJson(games)))
-    )
+    for {
+      games <- gamesRepo.list()
+      gamesWithPlayers = games.map(withPlayers)
+      sq <- Future.sequence(gamesWithPlayers)
+      gwp = sq.flatten
+    } yield {
+      Ok(Json.toJson(gwp))
+    }
   }
 
   def getGame(gameId: Long) = Action.async {
     gamesRepo.findById(gameId).map {
-      case Some(game) => Ok(Json.toJson(game))
+      case Some(game) => Ok(Json.toJson(GameWithPlayers(game, Seq())))
       case None => NotFound("No game with ID " + gameId)
+    }
+  }
+
+  def withPlayers(game: Game): Future[Option[GameWithPlayers]] = {
+    game.id match {
+      case Some(id) => playerRepo.findbyGame(id).map(ps => Some(GameWithPlayers(game, ps)))
+      case None => Future.successful(None)
+    }
+  }
+
+  case class GameWithPlayers(game: Game, players: Seq[Player])
+  implicit val gameWithPlayersWrites = new Writes[GameWithPlayers] {
+    override def writes(o: GameWithPlayers): JsValue = {
+      val game = o.game
+      val players = o.players.map(p => Json.obj(
+        "userId" -> Json.toJson(p.userId),
+        "position" -> Json.toJson(p.position),
+        "side" -> Json.toJson(p.side))
+      )
+
+      Json.obj(
+        "id" -> Json.toJson(game.id),
+        "tableId" -> Json.toJson(game.tableId),
+        "goalsHome" -> Json.toJson(game.goalsHome),
+        "goalsAway" -> Json.toJson(game.goalsAway),
+        "start" -> Json.toJson(game.start),
+        "end" -> Json.toJson(game.end),
+        "players" -> Json.toJson(players)
+      )
     }
   }
 
